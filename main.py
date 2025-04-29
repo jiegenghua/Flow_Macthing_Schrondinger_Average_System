5f29a15af61a7a2335dd35c73e31a7b6549e32d7
Mi Zhou <mi.jamie.zhou@gmail.com>
2025-04-29 19:06:08 -0400

    everything in torch

--- main.py
+++ main.py
@@ -5,16 +5,11 @@ from torch.distributions import MultivariateNormal
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from plot_results import plot_trajectories
+from plot_results import plot_trajectories, plot_initial_target_dis_2d
+
 '''
 Flow matching solver for stochatic averaged systems
 '''
-
-def avg_over_theta(func, thetas):
-    values = [func(theta) for theta in thetas]
-    return sum(values) / len(values)
-
-
 class FlowMatchingSolver:
     def __init__(self,
                  A_fn,
@@ -69,25 +64,26 @@ class FlowMatchingSolver:
 
     def compute_control_trajectories(self, x0, xf):
         # returns u_z trajectories: shape [N, Nt+1, dim]
-        N, nx = x0.shape
+        N, _ = x0.shape
         u_z = torch.zeros(N, self.Nt+1, self.nu, device=self.device)
-        for i in range(N): # for every sample
-            dW = torch.randn(self.Nt, nu, device=self.device) * math.sqrt(self.dt)
-            for k, t in enumerate(self.t_grid): # for every time step
-                Phi_tf_t = self.compute_Phi(self.tf, t)
-                G_tf_0 = self.compute_G(0)
-                Ginv = torch.linalg.inv(G_tf_0 + 1e-6 * torch.eye(nu, device=self.device))
-                integral_term = torch.zeros(nu, device=self.device)
-                for m, tau in enumerate(self.t_grid[:k]):
-                    Phi_tf_tau = self.compute_Phi(self.tf, tau)
-                    G_tf_tau = self.compute_G(tau)
-                    G_tf_tau_inv = torch.linalg.inv(G_tf_tau+1e-6*torch.eye(nu, device=self.device))
-                    integrand = Phi_tf_t.T @ G_tf_tau_inv @ Phi_tf_tau
-                    integral_term += integrand @ dW[m]
-                term1 = -math.sqrt(self.epsilon) * integral_term
-                M_t = self.M(self.tf)
-                term2 = Phi_tf_t.T @ Ginv @ (xf[i] - M_t @ x0[i])
-                u_z[i, k] = term1 + term2
+        dW = torch.randn(N, self.Nt, nu, device=self.device) * math.sqrt(self.dt)
+
+        for k, t in enumerate(self.t_grid): # for every time step
+            Phi_tf_t = self.compute_Phi(self.tf, t)
+            G_tf_0 = self.compute_G(0)
+            Ginv = torch.linalg.inv(G_tf_0 + 1e-6 * torch.eye(nu, device=self.device))
+            integral_term = torch.zeros(N, nu, device=self.device)
+            for m, tau in enumerate(self.t_grid[:k]):
+                Phi_tf_tau = self.compute_Phi(self.tf, tau)
+                G_tf_tau = self.compute_G(tau)
+                G_tf_tau_inv = torch.linalg.inv(G_tf_tau+1e-6*torch.eye(nu, device=self.device))
+                integrand = Phi_tf_t.T @ G_tf_tau_inv @ Phi_tf_tau
+                integral_term += dW[:, m]@integrand
+            term1 = -math.sqrt(self.epsilon) * integral_term
+            M_t = self.M(self.tf)
+            term2 = (xf - x0.matmul(M_t.T))@Ginv.T@Phi_tf_t
+            u_z[:, k] = term1 + term2
+        
         return u_z
         
     def M(self, t):
@@ -124,7 +120,7 @@ class FlowMatchingSolver:
                           hidden_dim=12,
                           lr=1e-3,
                           batch_size=128,
-                          epochs=20):
+                          epochs=200):
         input_dim = X.size(1)
         output_dim = Y.size(1)
         model = self.LSTMRegressor(input_dim, hidden_dim, output_dim).to(self.device)
@@ -151,12 +147,12 @@ class FlowMatchingSolver:
         x = x0.clone()
         traj = torch.zeros(N, self.Nt+1, nx, device=self.device)
         traj[:, 0, :] = x
-        dW = torch.randn(N, self.Nt, self.nu, device=self.device)
+        dW = torch.randn(N, self.Nt, self.nu, device=self.device)*math.sqrt(self.dt)
         for k, t in enumerate(self.t_grid[1:]): 
             noise_integral_term = torch.zeros([N, nx], device=self.device)
             for m, tau in enumerate(self.t_grid[:k+1]):
                 Phi_t_tau = self.compute_Phi(t, tau)
-                noise_integral_term += (Phi_t_tau @ dW[:, m].T).T*self.dt
+                noise_integral_term += (Phi_t_tau @ dW[:, m].T).T
             noise_term = math.sqrt(self.epsilon)*noise_integral_term
             features = torch.cat([x0, t.unsqueeze(0).repeat(N,1), noise_term], dim=1)
             u = self.control_model(features.unsqueeze(1))
@@ -174,6 +170,7 @@ if __name__ == "__main__":
     '''
     example 2
     '''
+    sys = 'Example2'
     nx = 2   # dimension of the state
     nu = 2
     A_fn = lambda theta: torch.tensor([[-theta, 0.0], [0.0, -theta]], device=device)
@@ -182,6 +179,7 @@ if __name__ == "__main__":
     '''
     example 1
     '''
+    # sys = 'Example1'
     #nx = 1 # dimension of x
     #nu =1  # dimension of u
     #A_fn = lambda theta: torch.tensor([[theta]], device=device)
@@ -194,9 +192,9 @@ if __name__ == "__main__":
     print(f"using device {device}")
     # algorithm part
     print("Initialize solver")
-    solver = FlowMatchingSolver(A_fn, B_fn, mu0, muf, nx, nu, epsilon=0.1, tf=1.0, time_steps=100, device=device)
+    solver = FlowMatchingSolver(A_fn, B_fn, mu0, muf, nx, nu, epsilon=0.01, tf=1.0, time_steps=10, device=device)
     print("Sample from initial and target distribution")
-    x0_samples, xf_samples = solver.sample_pairs(N=10)
+    x0_samples, xf_samples = solver.sample_pairs(N=100)
     print("Compute the control")
     u_z = solver.compute_control_trajectories(x0_samples, xf_samples)
     print("Prepare for the data set")
@@ -204,11 +202,11 @@ if __name__ == "__main__":
     print("Start LSTM training")
     solver.train_control_law(X, Y)
     print("Test and get the trajectory with learned control")
-    n_sample = 3
-    x0_test = torch.zeros([n_sample, nx], device=device)
-    traj, t_grid = solver.simulate_x(x0_test)
+    traj, t_grid = solver.simulate_x(x0_samples)
     print("Simulation done. Start to plot graph")
-    plot_trajectories(traj, t_grid)
+    save_dir = f'./results/{sys}/'
+    plot_trajectories(traj, t_grid, save_dir)
+    plot_initial_target_dis_2d(x0_samples, xf_samples, traj,t_grid, save_dir)
     print('Figure saved in the results folder')
 
 
--- plot_results.py
+++ plot_results.py
@@ -2,19 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import os
-def plot_trajectories(traj, t_grid):
-    log_dir = './results/'
+def plot_trajectories(traj, t_grid, log_dir):
     os.makedirs(log_dir, exist_ok=True)
 
     if torch.is_tensor(traj):
         traj = traj.detach().cpu().numpy()
         t_grid = t_grid.detach().cpu().numpy()
 
-    n_samples, time_steps, state_dim = traj.shape
+    n_samples, _, state_dim = traj.shape
     for dim in range(state_dim):
-        plt.figure()
+        plt.figure(figsize=(8,6))
         for sample in range(n_samples):
             plt.plot(t_grid, traj[sample,:, dim])
             plt.xlabel(r'$t$')
             plt.ylabel(fr'$x_{dim}$')
             plt.savefig(log_dir+f'x{dim}.jpg')
+
+
+def plot_initial_target_dis_2d(x0, xf, traj,t_grid, log_dir):
+    os.makedirs(log_dir, exist_ok=True)
+    if torch.is_tensor(traj):
+        x0 =x0.detach().cpu().numpy()
+        xf = xf.detach().cpu().numpy()
+        traj = traj.detach().cpu().numpy()
+        t_grid = t_grid.detach().cpu().numpy()
+
+    plt.figure(figsize=(8,6))
+    N, nx = x0.shape
+    for i in range(N):
+        plt.plot(traj[i,:,0], traj[i,:,1], color='m', alpha=0.1)
+        
+    plt.scatter(x0[:, 0], x0[:, 1], color='b', label='initial')
+    plt.scatter(xf[:,0],  xf[:,1], color='r', label='target')
+    plt.xlim(-10,10)
+    plt.ylim(-10,10)
+    plt.legend(loc=2)
+    plt.xlabel(r'$x_t(1)$')
+    plt.ylabel(r'$x_t(2)$')
+    plt.savefig(log_dir+f'traj.jpg')
